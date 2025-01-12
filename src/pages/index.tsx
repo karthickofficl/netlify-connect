51ffe3a5e261ab2e72bb414e4a053479d9e3ba37
karthickofficl <karthikraja@akkenna.com>
2025-01-12 11:03:01 +0530

    second push

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ import NewsSlider from "@/Components/Home/NewsSlider";
 export default function Home() {
   return (
     <>
-    {/* test push */}
+    {/* second push */}
       <div className="pt-16 lg:pt-20">
         <Banner />
         <Service />
